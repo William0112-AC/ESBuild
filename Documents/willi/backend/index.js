b729ce173d81c8116a6d501dc79cf2d6a81de3fe
Elizabeth <alex.26.azul@gmail.com>
2023-06-05 20:44:46 -0600

    instalacion express

---
+++ Documents/willi/backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app ONI express();
+const port - 3000;
+
+app.get('/', (req, res) => {
+  res.send( "Hola mi server en express');
+ }
+app.listen(port, () => {
+  console.log('MS port' +   port);
+}); 
+You, seconds ago Uncommitted changes
